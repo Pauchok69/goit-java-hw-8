151b07ac3ac2fb50cf28a41ee6cdf76a41e94535
Andy <andrew.zerko@gmail.com>
2022-01-24 00:57:19 +0200

    feat(MyArrayList): implement MyLinkedListInterface to MyLinkedList

--- src/main/java/goit/hw_8/MyLinkedList.java
+++ src/main/java/goit/hw_8/MyLinkedList.java
@@ -1,4 +1,28 @@
 package goit.hw_8;
 
-public class MyLinkedList {
+public class MyLinkedList<T> implements MyLinkedListInterface<T>{
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
---
+++ src/main/java/goit/hw_8/MyLinkedListInterface.java
@@ -0,0 +1,13 @@
+package goit.hw_8;
+
+public interface MyLinkedListInterface<T> {
+    void add(T value);
+
+    T get(int index);
+
+    T remove(int index);
+
+    void clear();
+
+    int size();
+}
