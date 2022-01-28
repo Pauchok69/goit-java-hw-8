5d82dcd7f3dceb5aa5a2735ad440170d3d27bbda
Andy <andrew.zerko@gmail.com>
2022-01-28 15:54:01 +0200

    feat(MyLinkedList): add MyLinkedList with next methods: add, get, size, clear, remove

--- src/main/java/goit/hw_8/my_linked_list/MyLinkedList.java
+++ src/main/java/goit/hw_8/my_linked_list/MyLinkedList.java
@@ -1,37 +1,103 @@
 package goit.hw_8.my_linked_list;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
-//    private header;
-//    private size;
-//    private prev;
-//    private next;
+    private int size = 0;
 
-    public MyLinkedList() {
-
-    }
+    private Node<T> head;
+    private Node<T> last;
 
     @Override
     public void add(T value) {
+        if (head == null) {
+            head = new Node<>(value, null, null);
+            last = head;
+        } else {
+            Node<T> newNode = new Node<>(value, last, null);
+            last.next = newNode;
+            last = newNode;
+        }
 
+        size++;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        Node<T> result = findNodeByIndex(index);
+
+        return result.value;
+    }
+
+    private Node<T> findNodeByIndex(int index) throws IndexOutOfBoundsException {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        Node<T> result;
+
+        if (index > size / 2) {
+            result = last;
+
+            for (int i = 0; i < size - index - 1; i++) {
+                result = result.prev;
+            }
+        } else {
+            result = head;
+
+            for (int i = 0; i < index; i++) {
+                result = result.next;
+            }
+        }
+        return result;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Node<T> nodeToRemove = findNodeByIndex(index);
+
+        if (size == 1) {
+            clear();
+
+            return nodeToRemove.value;
+        }
+        Node<T> prevNodeOfRemoved = nodeToRemove.prev;
+        Node<T> nextNodeOfRemoved = nodeToRemove.next;
+
+        if (prevNodeOfRemoved == null) {
+            nextNodeOfRemoved.prev = null;
+            head = nextNodeOfRemoved;
+        } else if (nextNodeOfRemoved == null) {
+            prevNodeOfRemoved.next = null;
+            last = prevNodeOfRemoved;
+        } else {
+            prevNodeOfRemoved.next = nextNodeOfRemoved;
+            nextNodeOfRemoved.prev = prevNodeOfRemoved;
+        }
+        size--;
+
+        return nodeToRemove.value;
     }
 
     @Override
     public void clear() {
-
+        head = null;
+        last = null;
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<T> {
+        private final T value;
+        Node<T> next;
+        Node<T> prev;
+
+        public Node(T value, Node<T> prev, Node<T> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
     }
 }
--- src/main/java/goit/hw_8/my_linked_list/MyLinkedListTests.java
+++ src/main/java/goit/hw_8/my_linked_list/MyLinkedListTests.java
@@ -1,32 +1,43 @@
 package goit.hw_8.my_linked_list;
 
+import java.util.LinkedList;
+
 public class MyLinkedListTests {
     public static void main(String[] args) {
         MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+        System.out.println("myLinkedList.size() = " + myLinkedList.size());
 
-        for (int i = 0; i < 9; i++) {
-            myLinkedList.add("String" + i);
+        for (int i = 0; i <= 99; i++) {
+            myLinkedList.add("Element-" + i);
         }
 
-        System.out.println("myLinkedList = " + myLinkedList);
-//        System.out.println("myLinkedList.size() = " + myLinkedList.size());
-//        System.out.println("myLinkedList.get(3) = " + myLinkedList.get(3));
-//        System.out.println("myLinkedList.remove(4) = " + myLinkedList.remove(4));
-//        System.out.println("myLinkedList = " + myLinkedList);
-//
-//        myLinkedList.add(null);
-//
-//        System.out.println("myLinkedList = " + myLinkedList);
-//        System.out.println("myLinkedList.size() = " + myLinkedList.size());
-//
-//        myLinkedList.clear();
-//
-//        System.out.println("myLinkedList = " + myLinkedList);
-//        System.out.println("myLinkedList.size() = " + myLinkedList.size());
-////        System.out.println("myLinkedList.get(3) = " + myLinkedList.get(3));
-//
-//        ArrayList<String> arrayList = new ArrayList<>();
-//
-//        arrayList.remove(33);
+        System.out.println("myLinkedList.size() = " + myLinkedList.size());
+
+        System.out.println("myLinkedList.get(0) = " + myLinkedList.get(0));
+        System.out.println("myLinkedList.get(1) = " + myLinkedList.get(1));
+        System.out.println("myLinkedList.get(49) = " + myLinkedList.get(49));
+        System.out.println("myLinkedList.get(50) = " + myLinkedList.get(50));
+        System.out.println("myLinkedList.get(51) = " + myLinkedList.get(51));
+        System.out.println("myLinkedList.get(55) = " + myLinkedList.get(55));
+        System.out.println("myLinkedList.get(99) = " + myLinkedList.get(99));
+
+        System.out.println("myLinkedList.get(33) = " + myLinkedList.get(33));
+        System.out.println("myLinkedList.remove(33) = " + myLinkedList.remove(33));
+        System.out.println("myLinkedList.size() = " + myLinkedList.size());
+        System.out.println("myLinkedList.get(33) = " + myLinkedList.get(33));
+
+        System.out.println("myLinkedList.get(0) = " + myLinkedList.get(0));
+        System.out.println("myLinkedList.remove(0) = " + myLinkedList.remove(0));
+        System.out.println("myLinkedList.size() = " + myLinkedList.size());
+        System.out.println("myLinkedList.get(0) = " + myLinkedList.get(0));
+
+        System.out.println("myLinkedList.get(97) = " + myLinkedList.get(97));
+        System.out.println("myLinkedList.remove(97) = " + myLinkedList.remove(97));
+        System.out.println("myLinkedList.size() = " + myLinkedList.size());
+        System.out.println("myLinkedList.get(96) = " + myLinkedList.get(96));
+
+        System.out.println("Clear");
+        myLinkedList.clear();
+        System.out.println("myLinkedList.size() = " + myLinkedList.size());
     }
 }
